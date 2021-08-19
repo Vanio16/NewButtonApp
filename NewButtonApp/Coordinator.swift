f448be423de70093824d8b24262b57e82e868682
Ivan Zakharov <vanio16@yandex.ru>
2021-08-19 14:55:40 +0600

    added second screen

---
+++ NewButtonApp/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  NewButtonApp
+//
+//  Created by Ivan Zakharov on 19/8/21.
+//
+
+import Foundation
---
+++ NewButtonApp/UIVIew+Sugar.swift
@@ -0,0 +1,8 @@
+//
+//  UIVIew+Sugar.swift
+//  NewButtonApp
+//
+//  Created by Ivan Zakharov on 19/8/21.
+//
+
+import Foundation
